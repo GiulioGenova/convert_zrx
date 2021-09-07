ba89a765b0dd04637513a8a2d57d750d8a062988
Giulio Genova <giulio.genova@eurac.edu>
2021-09-07 11:01:21 +0200

    first commit - v1 of the script with example data and converted files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Convet .zrx to .csv
+
+Utility to convert .zrx files from the Province of Bozen meteo stations to .csv
+
+## Version
+
+**convert_zrx_v1.sh** is target to a specific (old) version of the zrx format
+
+## Usage
+
+bash convert_zrx_v1.sh test.zrx
---
+++ convert_zrx_v1.sh
@@ -0,0 +1,33 @@
+IFS=$'\n'
+splitdir="./splitted_$(basename $1 .zrx)"
+outdir="./converted_$(basename $1 .zrx)"
+
+
+if [ ! -d "$splitdir" ]; then
+  mkdir "$splitdir"
+fi
+
+csplit "$1" '/Exported/' {*} -z -f "$splitdir/xx"
+
+if [ ! -d "$outdir" ]; then
+  mkdir "$outdir"
+fi
+
+for filename in  $splitdir/xx*; do
+echo $filename
+
+	SNAME=$(awk  -F'#|;\\*;|\\|\\*\\|' '{print $2}' <<< $(head -n 20 $filename | grep  SNAME) | grep SNAME | sed 's/SNAME//')
+	SANR=$(awk  -F'#|;\\*;|\\|\\*\\|' '{print $3}' <<< $(head -n 20 $filename | grep  SANR) | grep SANR | sed 's/SANR//')
+	TSNAME=$(awk  -F'#|;\\*;|\\|\\*\\|' '{print $3}' <<< $(head -n 20 $filename | grep  TSNAME) | grep TSNAME | sed 's/TSNAME//')
+	REXCHANGE=$(awk  -F'#|;\\*;|\\|\\*\\|' '{print $2}' <<< $(head -n 20 $filename | grep  REXCHANGE) | grep REXCHANGE | sed 's/REXCHANGE//')
+	DATEFIRST=$(head -n20 $filename | sed -e '/#/ { N; d; }' | sed  's/\s*$//' | sed 's/ /,/g' | head -n1  | awk -F',' '{print $1;}')
+        DATELAST=$(tail -n1 $filename  | awk -F' ' '{print $1;}')
+
+	    file=$SNAME"_"$SANR"_"$TSNAME"_"$REXCHANGE"_"$DATEFIRST"_"$DATELAST".csv"
+            echo "-------"
+            echo "Working on: $file"
+
+            cat $filename | sed -e '/#/ { N; d; }' | sed  's/\s*$//' | sed 's/ /,/g' > "$outdir/$file"
+
+done
+
